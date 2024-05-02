b51a78408cb66c72d82cd2358cb42eab0b032895
Jacob Strieb <jstrieb@users.noreply.github.com>
2024-05-01 23:26:23 -0400

    Remove "under construction" message

--- src/App.svelte
+++ src/App.svelte
@@ -296,15 +296,6 @@
   <img src="{favicon}" alt="Poker Chipper logo" />
 </div>
 
-<p
-  style:color="red"
-  style:margin="2em 1em 0 1em"
-  style:font-weight="bold"
-  style:text-align="center"
->
-  Warning: this code is actively under construction.
-</p>
-
 <div class="main">
   {#if !displayFromUrl}
     <h2>Instructions</h2>
